edf9320f23ad4c8607e21b530c61684667a4c0c9
wenston <329837810@qq.com>
2018-10-12 18:23:35 +0800

    样式修改

--- src/packages/scrollbar/Scrollbar.vue
+++ src/packages/scrollbar/Scrollbar.vue
@@ -16,104 +16,104 @@
 </template>
 
 <script>
-import ScrollbarY from './ScrollbarY'
-// import {debounce} from 'karma-ui/util/throttle_debounce'
-export default {
-  name: 'KScrollbar',
-  components: {
-    ScrollbarY
-  },
-  data(){
-    return {
-      speed: 50,
-      top: 0,
-      left: 0,
-      maxScrollTop:0,
-      maxScrollLeft: 0,
-      contentHeight:0,//内容高度
-      wrapperHeight: 0,//scrollbar组件最外部容器的高度
-      dragging: false,
-    }
-  },
-  methods: {
-    onDragging(thumbTop, h) {
-      let top = thumbTop * this.maxScrollTop / (100 - h)
-      this.top = top
-      this.dragging = true
+  import ScrollbarY from './ScrollbarY'
+  // import {debounce} from 'karma-ui/util/throttle_debounce'
+  export default {
+    name: 'KScrollbar',
+    components: {
+      ScrollbarY
     },
-    onWheel(e) {
-      this.scrollY(e.deltaY)
-      this.dragging = false
-    },
-    scrollY(y) {
-      const s = this.speed,
-        max = this.maxScrollTop
-      let top = 0
-      if(y>0) {
-        top = s + this.top
-      }else if(y<0){
-        top = this.top - s
-      }
-      if(top<0) {
-        top = 0
-      }else if(top>max) {
-        top = max
-      }
-      this.top = top
-      //滚动条thumb位置
-      this.$refs.y.scroll(top)
-
-    },
-    getSize() {
-      const el = this.$el,
-        content = this.$refs.content
+    data() {
       return {
-        elClientHeight: el.clientHeight,
-        elClientWidth: el.clientWidth,
-        contentClientHeight: content.clientHeight,
-        contentClientWidth: content.clientWidth,
+        speed: 50,
+        top: 0,
+        left: 0,
+        maxScrollTop: 0,
+        maxScrollLeft: 0,
+        contentHeight: 0, //内容高度
+        wrapperHeight: 0, //scrollbar组件最外部容器的高度
+        dragging: false,
       }
     },
-    init() {
-      const size = this.getSize()
-      console.log(size)
-      this.contentHeight = size.contentClientHeight
-      this.wrapperHeight = size.elClientHeight
-      const
-        maxScrollTop = this.contentHeight - this.wrapperHeight,
-        maxScrollLeft = size.contentClientWidth - size.elClientWidth
-      this.maxScrollTop = maxScrollTop < 0?0:maxScrollTop
-      this.maxScrollLeft = maxScrollLeft<0?0:maxScrollLeft
-    },
-    resetContentPosition() {
-      //调整content内容区域的marginTop
-      const maxScrollTop = this.maxScrollTop
-      const top = this.top
-      if(top>maxScrollTop) {
-        this.top = maxScrollTop
-      }
-      this.$refs.y.scroll(this.top)
-    },
-    reset() {
-      this.$nextTick(()=>{
-        this.init()
-        this.$nextTick(()=>{
-          this.resetContentPosition()
+    methods: {
+      onDragging(thumbTop, h) {
+        let top = thumbTop * this.maxScrollTop / (100 - h)
+        this.top = top
+        this.dragging = true
+      },
+      onWheel(e) {
+        this.scrollY(e.deltaY)
+        this.dragging = false
+      },
+      scrollY(y) {
+        const s = this.speed,
+          max = this.maxScrollTop
+        let top = 0
+        if (y > 0) {
+          top = s + this.top
+        } else if (y < 0) {
+          top = this.top - s
+        }
+        if (top < 0) {
+          top = 0
+        } else if (top > max) {
+          top = max
+        }
+        this.top = top
+        //滚动条thumb位置
+        this.$refs.y.scroll(top)
+
+      },
+      getSize() {
+        const el = this.$el,
+          content = this.$refs.content
+        return {
+          elClientHeight: el.clientHeight,
+          elClientWidth: el.clientWidth,
+          contentClientHeight: content.clientHeight,
+          contentClientWidth: content.clientWidth,
+        }
+      },
+      init() {
+        const size = this.getSize()
+        console.log(size)
+        this.contentHeight = size.contentClientHeight
+        this.wrapperHeight = size.elClientHeight
+        const
+          maxScrollTop = this.contentHeight - this.wrapperHeight,
+          maxScrollLeft = size.contentClientWidth - size.elClientWidth
+        this.maxScrollTop = maxScrollTop < 0 ? 0 : maxScrollTop
+        this.maxScrollLeft = maxScrollLeft < 0 ? 0 : maxScrollLeft
+      },
+      resetContentPosition() {
+        //调整content内容区域的marginTop
+        const maxScrollTop = this.maxScrollTop
+        const top = this.top
+        if (top > maxScrollTop) {
+          this.top = maxScrollTop
+        }
+        this.$refs.y.scroll(this.top)
+      },
+      reset() {
+        this.$nextTick(() => {
+          this.init()
+          this.$nextTick(() => {
+            this.resetContentPosition()
+          })
         })
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.init()
       })
+      window.addEventListener('resize', this.reset)
+    },
+    updated() {
+      this.reset()
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.reset)
     }
-  },
-  mounted() {
-    this.$nextTick(()=>{
-      this.init()
-    })
-    window.addEventListener('resize', this.reset)
-  },
-  updated() {
-    this.reset()
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize',this.reset)
   }
-}
 </script>
--- src/packages/scrollbar/ScrollbarY.vue
+++ src/packages/scrollbar/ScrollbarY.vue
@@ -11,85 +11,85 @@
 </template>
 
 <script>
-export default {
-  props: {
-    speed: {
-      type: Number,
-      default: 50
+  export default {
+    props: {
+      speed: {
+        type: Number,
+        default: 50
+      },
+      contentHeight: Number, //内容高度
+      wrapperHeight: Number, //scrollbar组件最外部容器的高度
     },
-    contentHeight:Number,//内容高度
-    wrapperHeight: Number,//scrollbar组件最外部容器的高度
-  },
-  data() {
-    return {
-      maxScrollTop: 0,
-      top: 0,
-      lastTop: 0,
-      start: 0,
-      dragging: false,
-    }
-  },
-  computed: {
-    height() {
-      if(this.contentHeight!==0) {
-        return this.wrapperHeight / this.contentHeight * 100
+    data() {
+      return {
+        maxScrollTop: 0,
+        top: 0,
+        lastTop: 0,
+        start: 0,
+        dragging: false,
       }
     },
-    show() {
-      return this.contentHeight - this.wrapperHeight > 0
-    }
-  },
-  methods: {
-    onJump(e) {
-      let y = e.clientY
-      // todo
-    },
-    onDown(e) {
-      this.dragging = true
-      this.start = e.clientY
-      this.lastTop = this.top
-      document.addEventListener('mousemove', this.onMove)
-      document.addEventListener('mouseup', this.onUp)
-    },
-    onMove(e) {
-      if(this.dragging) {
-        let d = e.clientY - this.start
-        let precentageD = d / this.wrapperHeight * 100
-        let top = precentageD + this.lastTop
-        if(top<0) {
-          top = 0
-        }else if(top>(100-this.height)) {
-          top = 100 - this.height
+    computed: {
+      height() {
+        if (this.contentHeight !== 0) {
+          return this.wrapperHeight / this.contentHeight * 100
         }
-        // console.log(top,this.top,precentageD)
-        
-        this.top = top
-        this.$emit('dragging', this.top, this.height)
+      },
+      show() {
+        return this.contentHeight - this.wrapperHeight > 0
       }
     },
-    onUp(e) {
-      this.dragging = false
-      document.removeEventListener('mousemove', this.onMove)
-      document.removeEventListener('mouseup', this.onUp)
-    },
-    scroll(ctop) {
-      this.top = ctop/this.maxScrollTop*(100-this.height)
+    methods: {
+      onJump(e) {
+        let y = e.clientY
+        // todo
+      },
+      onDown(e) {
+        this.dragging = true
+        this.start = e.clientY
+        this.lastTop = this.top
+        document.addEventListener('mousemove', this.onMove)
+        document.addEventListener('mouseup', this.onUp)
+      },
+      onMove(e) {
+        if (this.dragging) {
+          let d = e.clientY - this.start
+          let precentageD = d / this.wrapperHeight * 100
+          let top = precentageD + this.lastTop
+          if (top < 0) {
+            top = 0
+          } else if (top > (100 - this.height)) {
+            top = 100 - this.height
+          }
+          // console.log(top,this.top,precentageD)
 
-      // this.top = ctop/this.maxScrollTop*(100-this.height)
-      // console.log(ctop,this.top)
+          this.top = top
+          this.$emit('dragging', this.top, this.height)
+        }
+      },
+      onUp(e) {
+        this.dragging = false
+        document.removeEventListener('mousemove', this.onMove)
+        document.removeEventListener('mouseup', this.onUp)
+      },
+      scroll(ctop) {
+        this.top = ctop / this.maxScrollTop * (100 - this.height)
+
+        // this.top = ctop/this.maxScrollTop*(100-this.height)
+        // console.log(ctop,this.top)
+      },
+      calcMaxScrollTop() {
+        let max = this.contentHeight - this.wrapperHeight
+        this.maxScrollTop = max < 0 ? 0 : max
+      }
     },
-    calcMaxScrollTop() {
-      let max = this.contentHeight - this.wrapperHeight
-      this.maxScrollTop = max<0?0:max
-    }
-  },
-  watch: {
-    contentHeight() {
-      this.calcMaxScrollTop()
-    },
-    wrapperHeight() {
-      this.calcMaxScrollTop()
+    watch: {
+      contentHeight() {
+        this.calcMaxScrollTop()
+      },
+      wrapperHeight() {
+        this.calcMaxScrollTop()
+      }
     }
   }
-}
 </script>
