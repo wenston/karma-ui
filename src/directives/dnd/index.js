28681d7641d329ba4d69aa78feb11632cceed5e9
qps <744056574@qq.com>
2020-07-28 16:10:51 +0800

    add 一些文件的注释

--- src/directives/dnd/index.js
+++ src/directives/dnd/index.js
@@ -1,3 +1,10 @@
+/**
+* @name: 
+* @description: 拖拽
+* @author: 
+* @create: 2020/07/28 15:01:55
+* @update: 2020/07/28 15:01:55
+*/
 import bind from './bind.js'
 export default {
   install(Vue, opts) {
