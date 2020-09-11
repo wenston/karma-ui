ea8960dc0573ff9e580b2f63cde5acdf5ccd3839
qps <744056574@qq.com>
2020-09-11 13:45:27 +0800

    Merge branch 'plus' into wyeth
    # Conflicts:
    #	package-lock.json
    #	src/packages/select-tree/selectTree.jsx
    #	src/packages/tree/props.js

--- src/directives/clickoutside/index.js
+++ src/directives/clickoutside/index.js
@@ -1,3 +1,10 @@
+/**
+* @name: 
+* @description: 点击外面可以关闭弹窗
+* @author: 
+* @create: 2020/07/28 14:58:17
+* @update: 2020/07/28 14:58:17
+*/
 import clickoutside from './clickoutside.js'
 export default {
   install(Vue, opts) {
