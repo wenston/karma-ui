7248c4f4cf142c3f04dc422298e1433198ff17aa
wenyongjiang <329837810@qq.com>
2020-09-27 11:36:19 +0800

    改样式

--- src/packages/dialog/index.js
+++ src/packages/dialog/index.js
@@ -1,12 +1,13 @@
-import KPopup from "karma-ui/packages/dialog/dialog"
+import KPopup from 'karma-ui/packages/dialog/dialog';
 
 export default {
   install(Vue, opts) {
-    const KPopupConstructor = Vue.extend(KPopup)
+    const KPopupConstructor = Vue.extend(KPopup);
     // let instance = new KPopupConstructor().$mount()
     // document.body.appendChild(instance.$el)
 
-    Vue.dialog = Vue.prototype.$dialog = function ({
+    Vue.dialog = Vue.prototype.$dialog = function({
+      parentNode = document.body,
       title,
       content, //接受字符串或者一个返回jsx的函数
       ok,
@@ -17,57 +18,57 @@ export default {
       hasIcon = true,
       iconColor,
       iconSize,
-      hasBottomLine = false
+      hasBottomLine = false,
     }) {
-      let instance = new KPopupConstructor().$mount()
-      instance.$props.title = title
+      let instance = new KPopupConstructor().$mount();
+      instance.$props.title = title;
 
-      instance.$props.okText = okText
+      instance.$props.okText = okText;
 
-      instance.$props.cancelText = cancelText
-      instance.$props.layout = layout
-      instance.$props.hasIcon = hasIcon
-      instance.$props.iconColor = iconColor
-      instance.$props.iconSize = iconSize
-      instance.$props.hasBottomLine = hasBottomLine
+      instance.$props.cancelText = cancelText;
+      instance.$props.layout = layout;
+      instance.$props.hasIcon = hasIcon;
+      instance.$props.iconColor = iconColor;
+      instance.$props.iconSize = iconSize;
+      instance.$props.hasBottomLine = hasBottomLine;
 
-      if (typeof content === "function") {
-        instance.$slots.default = content.call(this)
+      if (typeof content === 'function') {
+        instance.$slots.default = content.call(this);
       } else {
-        instance.$props.content = content
+        instance.$props.content = content;
       }
-      instance.$props.show = true
+      instance.$props.show = true;
 
-      instance.$on("after-ok", () => {
-        instance.$props.show = false
-        ok && ok()
-        instance.$off(["after-cancel", "after-ok"])
+      instance.$on('after-ok', () => {
+        instance.$props.show = false;
+        ok && ok();
+        instance.$off(['after-cancel', 'after-ok']);
         Vue.nextTick(() => {
-          if (instance.$el && document.body.contains(instance.$el)) {
-            document.body.removeChild(instance.$el)
+          if (instance.$el && parentNode.contains(instance.$el)) {
+            parentNode.removeChild(instance.$el);
           }
-        })
-      })
-      instance.$on("after-cancel", () => {
-        cancel && cancel()
-        instance.$off(["after-cancel", "after-ok"])
+        });
+      });
+      instance.$on('after-cancel', () => {
+        cancel && cancel();
+        instance.$off(['after-cancel', 'after-ok']);
         Vue.nextTick(() => {
-          if (instance.$el && document.body.contains(instance.$el)) {
-            document.body.removeChild(instance.$el)
+          if (instance.$el && parentNode.contains(instance.$el)) {
+            parentNode.removeChild(instance.$el);
           }
-        })
-      })
+        });
+      });
       instance.hide = () => {
-        instance.$props.show = false
-        instance.$off(["after-cancel", "after-ok"])
+        instance.$props.show = false;
+        instance.$off(['after-cancel', 'after-ok']);
         Vue.nextTick(() => {
-          if (instance.$el && document.body.contains(instance.$el)) {
-            document.body.removeChild(instance.$el)
+          if (instance.$el && parentNode.contains(instance.$el)) {
+            parentNode.removeChild(instance.$el);
           }
-        })
-      }
-      document.body.appendChild(instance.$el)
-      return instance
-    }
-  }
-}
+        });
+      };
+      parentNode.appendChild(instance.$el);
+      return instance;
+    };
+  },
+};
