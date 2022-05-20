6681804adfd395671473711fbde7c5a2bc7190e2
wenyongjiang <8d386e69f412d7b>
2022-05-20 09:22:54 +0800

    table2

--- src/packages/table2/table2.jsx
+++ src/packages/table2/table2.jsx
@@ -1,41 +1,41 @@
 // 在table基础上做的扩展，支持占位表格行
-import KTable from "karma-ui/packages/table/table"
-import KKeyEnter from "karma-ui/packages/key-enter/keyEnter"
+import KTable from "karma-ui/packages/table/table";
+import KKeyEnter from "karma-ui/packages/key-enter/keyEnter";
 export default {
   name: "KTable2",
   components: {
     KTable,
-    KKeyEnter
+    KKeyEnter,
   },
   model: {
     prop: "data",
-    event: "dataChange"
+    event: "dataChange",
   },
   props: {
     ...KTable.props,
     loopKey: {
       type: String,
-      default: "__karma__loop__key__"
+      default: "__karma__loop__key__",
     },
     stripe: {
       type: Boolean,
-      default: false
+      default: false,
     },
     hasIndex: {
       type: Boolean,
-      default: true
+      default: true,
     },
     hasAction: {
       type: Boolean,
-      default: true
+      default: true,
     },
     resizeWidth: {
       type: Boolean,
-      default: true
+      default: true,
     },
     minContent: {
       type: Boolean,
-      default: true
+      default: true,
     },
     //rowData可以不传，
     //但需注意：如果不传的话，当单元格内包含有其他组件时，将会出现组件
@@ -45,10 +45,10 @@ export default {
     //建议写一个函数返回一个对象
     rowData: {
       type: [Object, Function],
-      default: () => ({})
+      default: () => ({}),
     },
     //beforeDeleteRow是能返回Promise的函数
-    beforeDeleteRow: Function
+    beforeDeleteRow: Function,
     //占位行数，当不足时自动补充空行
     // placeholderRows: {
     //   type: Number,
@@ -57,65 +57,65 @@ export default {
   },
   data() {
     return {
-      rowkey: 10000
-    }
+      rowkey: 10000,
+    };
   },
   computed: {
     //对this.data进行加工，添加循环所需要的key,
     //默认以__karma__loop__key__作为键
     processData() {
       if (this.data && this.data.length) {
-        this.data.forEach(d => {
-          if (!(this.loopKey in d)) d[this.loopKey] = this.createLoopKey()
-        })
-        return this.data
+        this.data.forEach((d) => {
+          if (!(this.loopKey in d)) d[this.loopKey] = this.createLoopKey();
+        });
+        return this.data;
       }
-    }
+    },
   },
   methods: {
     createLoopKey() {
-      return "" + this.rowkey--
+      return "" + this.rowkey--;
     },
     setHighlightRow(e) {
-      this.$refs.table.setHighlightRow(e)
+      this.$refs.table.setHighlightRow(e);
     },
     getRowData(e) {
       let d =
-        typeof this.rowData === "function" ? this.rowData(e) : this.rowData
-      return JSON.parse(JSON.stringify(d))
+        typeof this.rowData === "function" ? this.rowData(e) : this.rowData;
+      return JSON.parse(JSON.stringify(d));
     },
     //外部调用
     init() {
       if (this.$refs.kke) {
-        return this.$refs.kke.init()
+        return this.$refs.kke.init();
       }
     },
     //外部调用
     focus(row, index) {
       if (this.$refs.kke) {
-        return this.$refs.kke.focus(row, index)
+        return this.$refs.kke.focus(row, index);
       }
     },
     //外部调用
     next(n) {
       if (this.$refs.kke) {
-        return this.$refs.kke.next(n)
+        return this.$refs.kke.next(n);
       }
     },
     unshiftRow() {
-      this.data.unshift(this.getRowData())
+      this.data.unshift(this.getRowData());
     },
     pushRow(row) {
-      this.data.splice(row + 1, 0, this.getRowData())
+      this.data.splice(row + 1, 0, this.getRowData());
     },
     scrollTo(obj) {
       if (this.$refs.table) {
-        this.$refs.table.scrollTo(obj)
+        this.$refs.table.scrollTo(obj);
       }
     },
     scrollIntoViewIfNeed(elem) {
-      if (this.$refs.table) this.$refs.table.scrollIntoViewIfNeed(elem, 40)
-    }
+      if (this.$refs.table) this.$refs.table.scrollIntoViewIfNeed(elem, 40);
+    },
   },
   render() {
     const tableProps = {
@@ -123,22 +123,21 @@ export default {
       ref: "table",
       props: {
         ...this.$props,
-        data: this.processData
+        data: this.processData,
       },
       scopedSlots: {
-        ...this.$scopedSlots
+        ...this.$scopedSlots,
       },
       on: {
-        ...this.$listeners,
         // "click-row": ({row,index})=> {
         //   this.data.splice(index, 1, this.getRowData({row,index}))
         //   this.$emit("add-row", {row,index})
         // },
-        "add-row": e => {
-          this.data.splice(e.index + 1, 0, this.getRowData(e))
-          this.$emit("add-row", e)
+        "add-row": (e) => {
+          this.data.splice(e.index + 1, 0, this.getRowData(e));
+          this.$emit("add-row", e);
         },
-        "delete-row": e => {
+        "delete-row": (e) => {
           if (this.data.length > 1) {
             // this.$emit('dataChange', this.data.splice(e.index,1))
             if (
@@ -147,52 +146,53 @@ export default {
             ) {
               this.beforeDeleteRow(e)
                 .then(() => {
-                  this.data.splice(e.index, 1)
-                  this.$emit("delete-row", e)
+                  this.data.splice(e.index, 1);
+                  this.$emit("delete-row", e);
                 })
-                .catch(() => {})
+                .catch(() => {});
             } else {
-              this.data.splice(e.index, 1)
-              this.$emit("delete-row", e)
+              this.data.splice(e.index, 1);
+              this.$emit("delete-row", e);
             }
           }
-        }
-      }
-    }
+        },
+        ...this.$listeners,
+      },
+    };
     return (
       <k-key-enter
         ref="kke"
-        onEnd={e => {
-          if (this.hasAction && this.actions.some(a => a == "add")) {
-            const [row, i] = e
+        onEnd={(e) => {
+          if (this.hasAction && this.actions.some((a) => a == "add")) {
+            const [row, i] = e;
             if (row === 0 && i === 0) {
-              this.unshiftRow()
+              this.unshiftRow();
             } else {
-              this.pushRow(row)
+              this.pushRow(row);
             }
           }
         }}
-        onEnd-row={e => {
-          if (this.hasAction && this.actions.some(a => a == "add")) {
-            const [row, i] = e
+        onEnd-row={(e) => {
+          if (this.hasAction && this.actions.some((a) => a == "add")) {
+            const [row, i] = e;
             if (row === 0) {
-              this.unshiftRow()
+              this.unshiftRow();
             } else {
-              this.pushRow(row)
+              this.pushRow(row);
             }
           }
         }}
       >
         <k-table {...tableProps} />
       </k-key-enter>
-    )
+    );
   },
   updated() {
     // console.log('table2 updated')
-    setTimeout(()=>{
-      if(this.$refs.table) {
-        this.$refs.table.init()
+    setTimeout(() => {
+      if (this.$refs.table) {
+        this.$refs.table.init();
       }
-    })
-  }
-}
+    });
+  },
+};
